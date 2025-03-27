07db567b2fb156d0c4878daf38010056a7c7a5ac
rustam sagaddinov <rustiksagad@gmail.com>
2025-03-26 22:04:20 -0300

    first commit

---
+++ src/main/java/org/example/model/Match.java
@@ -0,0 +1,49 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "matches")
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "player1_id", nullable = false)
+    private Player player1;
+
+    @ManyToOne
+    @JoinColumn(name = "player2_id", nullable = false)
+    private Player player2;
+
+    @Column(nullable = false)
+    private Integer score1;
+
+    @Column(nullable = false)
+    private Integer score2;
+
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Player getPlayer1() { return player1; }
+    public void setPlayer1(Player player1) { this.player1 = player1; }
+
+    public Player getPlayer2() { return player2; }
+    public void setPlayer2(Player player2) { this.player2 = player2; }
+
+    public Integer getScore1() { return score1; }
+    public void setScore1(Integer score1) { this.score1 = score1; }
+
+    public Integer getScore2() { return score2; }
+    public void setScore2(Integer score2) { this.score2 = score2; }
+
+    public LocalDateTime getDateTime() { return dateTime; }
+    public void setDateTime(LocalDateTime dateTime) { this.dateTime = dateTime; }
+}
---
+++ src/main/java/org/example/model/Player.java
@@ -0,0 +1,23 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+}
